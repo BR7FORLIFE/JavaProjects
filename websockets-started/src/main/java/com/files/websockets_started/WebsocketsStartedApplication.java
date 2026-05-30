967628a184940b737d78b47abba3fe93f46560f0
BR7FORLIFE <diazpachecobrayan@gmail.com>
2026-05-29 19:24:29 -0500

    feat: started project with spring

---
+++ websockets-started/src/main/java/com/files/websockets_started/WebsocketsStartedApplication.java
@@ -0,0 +1,13 @@
+package com.files.websockets_started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsStartedApplication.class, args);
+	}
+
+}
---
+++ websockets-started/src/test/java/com/files/websockets_started/WebsocketsStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.files.websockets_started;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
